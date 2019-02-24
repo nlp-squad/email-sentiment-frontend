4bf05bd63fec6875ce8046e65ec2126b9429f30e
blessed-tawanda <blessedmahuni@gmail.com>
2019-02-24 19:36:26 +0200

    added pre loader on the search

--- src/components/TwitterStatistics.vue
+++ src/components/TwitterStatistics.vue
@@ -15,6 +15,27 @@
     <div v-if="showchart" class="chart-area">
       <chart v-if="showchart" :chart-data="chartdata" :options="options"></chart>
     </div>
+    <div v-if="loading" class="center">
+      <div class="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <span class="loader-text">
+          Fetching And Analysing Data
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +50,7 @@ export default {
   data () {
     return {
       showchart: false,
+      loading: false,
       positive: 0,
       negative: 0,
       neutral: 0,
@@ -46,7 +68,8 @@ export default {
   },
   methods: {
     search () {
-      console.log('Searching')
+      this.loading = true
+      this.showchart = false
       var data = {
         query: this.query,
         count: 100000
@@ -66,6 +89,7 @@ export default {
           ]
         }
         this.showchart = true
+        this.loading = false
       }).catch((err) => {
         console.log(err)
       })
@@ -78,7 +102,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .center {
+    position: absolute;
+    left: 40%;
+    text-align: center
+  }
+  .loader-text {
+    font-size: 2rem;
+  }
   .chart {
+    position: relative;
     &-search-bar {
       background-color: #faf9f9;
       padding: 0 1rem;
@@ -128,4 +161,88 @@ export default {
       padding: 2rem;
     }
   }
+
+  .lds-default {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-default div {
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  background: #039BE5;
+  border-radius: 50%;
+  animation: lds-default 1.2s linear infinite;
+}
+.lds-default div:nth-child(1) {
+  animation-delay: 0s;
+  top: 29px;
+  left: 53px;
+}
+.lds-default div:nth-child(2) {
+  animation-delay: -0.1s;
+  top: 18px;
+  left: 50px;
+}
+.lds-default div:nth-child(3) {
+  animation-delay: -0.2s;
+  top: 9px;
+  left: 41px;
+}
+.lds-default div:nth-child(4) {
+  animation-delay: -0.3s;
+  top: 6px;
+  left: 29px;
+}
+.lds-default div:nth-child(5) {
+  animation-delay: -0.4s;
+  top: 9px;
+  left: 18px;
+}
+.lds-default div:nth-child(6) {
+  animation-delay: -0.5s;
+  top: 18px;
+  left: 9px;
+}
+.lds-default div:nth-child(7) {
+  animation-delay: -0.6s;
+  top: 29px;
+  left: 6px;
+}
+.lds-default div:nth-child(8) {
+  animation-delay: -0.7s;
+  top: 41px;
+  left: 9px;
+}
+.lds-default div:nth-child(9) {
+  animation-delay: -0.8s;
+  top: 50px;
+  left: 18px;
+}
+.lds-default div:nth-child(10) {
+  animation-delay: -0.9s;
+  top: 53px;
+  left: 29px;
+}
+.lds-default div:nth-child(11) {
+  animation-delay: -1s;
+  top: 50px;
+  left: 41px;
+}
+.lds-default div:nth-child(12) {
+  animation-delay: -1.1s;
+  top: 41px;
+  left: 50px;
+}
+@keyframes lds-default {
+  0%, 20%, 80%, 100% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+}
+
 </style>
