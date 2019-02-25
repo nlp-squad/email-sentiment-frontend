cc2a7f395ab5b2330c87033ed201faec06a48cf0
blessed-tawanda <blessedmahuni@gmail.com>
2019-02-25 21:52:21 +0200

    Added backend functionallity for real time tweets

--- src/components/RealtimeTweets.vue
+++ src/components/RealtimeTweets.vue
@@ -9,27 +9,48 @@
             class="icon">
       </svg>
     </button>
-    <input v-model="query" class="chart-search-bar-input" type="text" name="search-criteria" id="" placeholder="Name of Product e.g. Samsung S6 Edge">
-    <button @click="search" class="chart-search-bar-button"> Look Up Statistics </button>
+    <input v-model="tweetString" class="chart-search-bar-input" type="text" name="search-criteria" id="" placeholder="Name of Product e.g. Samsung S6 Edge">
+    <button @click="trackTweets" class="chart-search-bar-button"> Look Up Statistics </button>
   </div>
   <div class="tweet-container">
-    <div v-for="(i,k) in 50" :key="k" class="tweet">
-      <div class="tweet-image-container">
-        <img class="tweet-image" src="./../assets/logo.png" alt="">
-      </div>
+    <div v-for="(tweet,k) in tweets" :key="k" class="tweet">
+      <img class="tweet-image" :src="tweet.picture" alt="user-profile">
       <div class="tweet-body">
-        <p> {{i}} I love going to the OK branch in Belvedere </p>
+        <p class="tweet-body-name"> {{`${tweet.screenName}:`}}  </p>
+        <!-- <p class="tweet-body-text"> {{tweet.text}} </p> -->
+        <div class="tweet-body-text" v-html="tweet.text"></div>
       </div>
     </div>
   </div>
 </div>
 </template>
-
 <script>
+// import Vue from 'vue'
+// import VueSocket from 'vue-socket.io'
+// Vue.use(VueSocket, 'http://127.0.0.1:40')
+
 export default {
+  data () {
+    return {
+      tweetString: '',
+      tweets: []
+    }
+  },
   methods: {
     back () {
       this.$router.go(-1)
+    },
+    trackTweets () {
+      var data = {
+        tweetString: this.tweetString
+      }
+      console.log(data)
+      this.$socket.emit('trackthis', data)
+    }
+  },
+  sockets: {
+    newtweet: function (data) {
+      this.tweets.push(data)
     }
   }
 }
@@ -46,29 +67,37 @@ export default {
 
     &-body {
       padding: 1rem;
+
+      &-name {
+        color: #42A5F5;
+        font-weight: bold;
+      }
+
+      &-text {
+
+      }
     }
 
     &-image {
       height: 5rem;
-
+       border-radius: 100%;
       &-container {
-        // width: 5rem;
+        width: 10rem;
         overflow: hidden;
-        border-radius: 100%;
         background-color: #fff;
       }
     }
 
     &-container {
       display: flex;
-      flex-direction: column;
+      flex-direction: column-reverse;
       height: 100%;
       margin: 0 auto;
       justify-items: flex-end;
       width: 70%;
     }
   }
-  .tweet:not(:last-child) {
+  .tweet {
     margin-bottom: 2rem;
   }
 
